a77fdaae2e93e9217f81159fd68eb18b6746f482
Nikita <popovn2268@gmail.com>
2025-02-23 22:44:05 +0300

    upd: docker

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+User.objects.filter(username='admin').exists() or \
+User.objects.create_superuser('admin', 'admin@example.com', 'admin')" \
+| python manage.py shell
+
+exec "$@"
